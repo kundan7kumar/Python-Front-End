f958f5b8b0f7d50469a30c776a6ea8836b084539
kukumar <kukumar@softlayer.com>
2019-02-14 12:34:13 -0600

    updated

--- Flask_Visual/app.py
+++ Flask_Visual/app.py
@@ -1,1 +1,23 @@
-import flask from Flask
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    #return '<h1>Hello World!</h1>'
+    browser_info=request.headers.get('User-Agent')
+    return '<h1> The browser info: {} </h1>'.format(browser_info)
+
+@app.route('/information')
+def info():
+    return '<h1>Cool Guys</h1>'
+
+@app.route('/info/<name>')
+def name(name):
+    return 'My name is {}'.format(name)
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555,debug=True)
+
